71da29e62bcbc82862cecb5f3bd819dd23f273cf
Matthias Huber <matthias.huber@stud.hslu.ch>
2014-02-25 11:53:21 +0100

    Assignment #5: Processor Expert with the FRDM

--- FRDM/Sources/Events.h
+++ FRDM/Sources/Events.h
@@ -34,6 +34,10 @@
 #include "PE_Error.h"
 #include "PE_Const.h"
 #include "IO_Map.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "WAIT1.h"
 
 #ifdef __cplusplus
 extern "C" {
--- FRDM/Sources/ProcessorExpert.c
+++ FRDM/Sources/ProcessorExpert.c
@@ -30,6 +30,10 @@
 /* Including needed modules to compile this module/procedure */
 #include "Cpu.h"
 #include "Events.h"
+#include "LED1.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "WAIT1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
@@ -49,6 +53,13 @@ int main(void)
   /*** End of Processor Expert internal initialization.                    ***/
 
   /* Write your code here */
+  for (;;)
+  {
+	  LED1_On();
+	  WAIT1_Waitms(500);
+	  LED1_Off();
+	  WAIT1_Waitms(500);
+}
   /* For example: for(;;) { } */
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
